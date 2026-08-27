7afbfc7efb964f6942d2cf1fce38924d5d213c35
ankit-infinity <ghoshankit15325@gmail.com>
2026-08-27 20:00:26 +0530

    change in some forecastmodel

--- lakehuron.R
+++ lakehuron.R
@@ -8,6 +8,8 @@ class(LakeHuron)
 
 print(dim(data.frame(LakeHuron)))
 
+length(LakeHuron)
+
 plot(LakeHuron,main='lakehuron water level',ylab='feet',col="blue",lwd= 2)
 
 lake_train=window(LakeHuron,start=1875,end=1950)
@@ -17,7 +19,7 @@ lake_test=window(LakeHuron,start=1951,end=1972)
 
 library(tseries)
 adf_test=adf.test(lake_train)
-adf_test
+print(adf_test)
 
 #making the data stationary
 diff_lake=diff(lake_train)
@@ -36,8 +38,9 @@ par(mfrow=c(1,1))
 
 
 # arima model manual parameter selection using acf pacf
-#arima(1,1,2)
-#library(forecast)
+# arima(1,1,2)
+# library(forecast)
+
 manual=arima(lake_train,order =c(1,1,2))
 print(manual)
 
@@ -61,27 +64,61 @@ checkresiduals(manual)
 checkresiduals(manual_3)
 
 #actual vs fitted plot
-plot(fitted(manual_3),col='red',main='arima(0,1,0) fitted vs actual')
+plot(fitted(manual),col='red',main='arima(1,1,2) fitted vs actual')
 lines(lake_train,col='blue') 
 legend("topright",legend = c('fitted','actual'),fill=c('red','blue'))
 
 #forecast plot
-forecast_lake=forecast(manual_3,h=22,level=c(85,95))
-autoplot(forecast_lake,main='arima(0,1,0)forecast')
- 
- 
-plot(lake_test,col='black',main='forecast vs test data in arima(0,1,0)')
+forecast_lake=forecast(manual,h=length(lake_test),level=c(85,95))
+autoplot(forecast_lake,main='arima(1,1,2)forecast')
+
+plot
+plot(lake_test,col='black',main='forecast vs test data in arima(1,1,2)')
 lines(forecast_lake$mean,col='red')
-legend('topright',legend = c('forecast','actual'),fill=c('red','black'))
+legend('bottomright',legend = c('forecast','actual'),fill=c('red','black'))
+
+
 
 #model comaparison
 accuracy=forecast_lake$mean-lake_test
+rmse=sqrt(mean(accuracy^2))
+print(rmse)
 cat("test error n 22 data points",sum(accuracy^2),'\n')
+cat('train_error',manual$sigma2)
+
+forecast_lake_1=forecast(manual_3,h=22,level=c(85,95))
+accuracy_1=forecast_lake_1$mean-lake_test
+rmse_3=sqrt(mean(accuracy_1^2))
+print(rmse_3)
+cat("test error n 22 data points",sum(accuracy_1^2),'\n')
 cat('train_error',manual_3$sigma2)
 
-forecast_lake_1=forecast(manual,h=22,level=c(85,95))
-accuracy_1=forecast_lake_1$mean-lake_test
-cat("test error n 22 data points",sum(accuracy_1^2),'\n')
-cat('train_error',manual$sigma2)
+forecast_comparison=data.frame(
+  
+  model=c(
+    "arima(0,1,0)",
+    
+    "arima(1,1,2)"
+  ),
+  AIC=c(
+    
+    AIC(manual_3),
+    AIC(manual)
+    
+  ),
+  
+  RMSE=c(
+    
+    rmse_3,
+    rmse
+    
+    )
+  )
+
+print(forecast_comparison)
+
+
+
+
 
 
